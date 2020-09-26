6e17f8d0b1972a4709273673b287cba93da95bca
Thomas Bach <thomas.bach@email.de>
2020-09-26 02:02:36 +0200

    add sources for triangle example

---
+++ src/main/java/triangle/TriangleTbach.java
@@ -0,0 +1,11 @@
+package triangle;
+
+public class TriangleTbach implements Triangle {
+
+	@Override
+	public TriangleResult getTriangleResultForLengths(int... lengths) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
