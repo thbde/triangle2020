86e6cd798912447c4de72081bd20ebf7a51c39b7
Sebastian Milchsack <sebastian.milchsack@msc-gmbh.net>
2020-09-26 16:14:07 +0200

    Lösung Breakout-Session 3, in das richtige Verzeichnis geschoben

--- src/main/java/triangle/implementations/smilchsack/TriangleSmilchsack.java
+++ src/main/java/triangle/implementations/smilchsack/TriangleSmilchsack.java
@@ -3,11 +3,44 @@ package triangle.implementations.smilchsack;
 import triangle.Triangle;
 import triangle.TriangleResult;
 
-public class TriangleSmilchsack implements Triangle {
-
+public class TriangleSMilchsack implements Triangle
+{
 	@Override
-	public TriangleResult getTriangleResultForLengths(int... lengths) {
+	public TriangleResult getTriangleResultForLengths(int... lengths)
+	{
+		// Improved
+		if (lengths == null || lengths.length < 3 || lengths.length > 3)
+		{
+			return TriangleResult.UNKNOWN;
+		}
+
+		final int a = lengths[0];
+		final int b = lengths[1];
+		final int c = lengths[2];
+
+		// Added
+		if (a <= 0 || b <= 0 || c <= 0)
+		{
+			return TriangleResult.UNKNOWN;
+		}
+
+		// Added
+		if (a >= (b + c) || c >= (b + a) || b >= (a + c))
+		{
+			return TriangleResult.UNKNOWN;
+		}
+
+		if (a == b && b == c)
+		{
+			return TriangleResult.TRIANGLE_EQUILATERAL;
+		}
+
+		// Edit
+		if (a == b || b == c || a == c) 
+		{
+			return TriangleResult.TRIANGLE_ISOSCELES;
+		}
+
 		return TriangleResult.TRIANGLE_NOT_EQUAL;
 	}
-
 }
