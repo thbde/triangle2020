84f7fbc04576c29ebdffaab2136fe2e2447c2cda
Etienne Dehm <etde0001@stud.hs-kl.de>
2020-09-26 15:08:40 +0200

    Triangle Dehm

--- src/main/java/triangle/implementations/edehm/TriangleEdehm.java
+++ src/main/java/triangle/implementations/edehm/TriangleEdehm.java
@@ -7,7 +7,38 @@ public class TriangleEdehm implements Triangle {
 
 	@Override
 	public TriangleResult getTriangleResultForLengths(int... lengths) {
-		return TriangleResult.TRIANGLE_NOT_EQUAL;
+		if (lengths == null) {
+			return TriangleResult.UNKNOWN;
+		}
+		final int a = lengths[0];
+		final int b = lengths[1];
+		final int c = lengths[2];
+
+		if ( a <= 0 || b <= 0 || c <= 0) {
+			return TriangleResult.UNKNOWN;
+		}
+		else if (a == b && b == c && a ==c) {
+			return TriangleResult.TRIANGLE_EQUILATERAL;
+		}
+		else if (a == b || b == c || c == a) {
+			return TriangleResult.TRIANGLE_ISOSCELES;
+		}
+		else {
+			return TriangleResult.TRIANGLE_NOT_EQUAL;
+		}
+	}
+
+	public TriangleResult getTriangleResultForLengths(int a, int b, int c) {
+		
+		if (a == b && b == c && a ==c) {
+			return TriangleResult.TRIANGLE_EQUILATERAL;
+		}
+		else if (a == b || b == c || c == a) {
+			return TriangleResult.TRIANGLE_ISOSCELES;
+		}
+		else {
+			return TriangleResult.TRIANGLE_NOT_EQUAL;
+		}
 	}
 
 }
